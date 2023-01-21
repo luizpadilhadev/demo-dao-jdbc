8b51e547311131e9a347793a9d9aa22691173cc8
luizpadilhadev <luizfelipepadilha86@gmail.com>
2023-01-21 11:33:13 -0300

    update implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -30,5 +30,15 @@ public class Main {
         for (Seller obj : list) {
             System.out.println(obj);
         }
+        System.out.println("\n=== TEST 4: seller Insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! new id = " + newSeller.getId());
+
+        System.out.println("\n=== TEST 5: seller Update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
     }
 }
